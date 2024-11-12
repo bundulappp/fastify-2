8cd22fc488999ece01a17ade9ecb9807925be18e
Bundula Patrik <bundulappp@gmail.com>
2024-11-12 10:57:02 +0100

    add jsonschema provider to routes

--- src/routes/pet.routes.ts
+++ src/routes/pet.routes.ts
@@ -1,73 +1,40 @@
-import { FastifyInstance } from 'fastify';
+import { FastifyInstance, FastifyRequest, FastifyReply } from 'fastify';
+import { JsonSchemaToTsProvider } from '@fastify/type-provider-json-schema-to-ts';
 import { PetService } from '../service/pet.service';
-
-const petSchema = {
-  type: 'object',
-  properties: {
-    id: { type: 'number' },
-    name: { type: 'string' },
-    age: { type: 'number' },
-    weightInKg: { type: 'number' },
-  },
-};
-
-export const petResponseSchema = {
-  200: {
-    petSchema,
-  },
-} as const;
-
-export const getAllPetResponseSchema = {
-  200: {
-    type: 'array',
-    items: petSchema,
-  },
-};
-
-export const petPostBodyRequestSchema = {
-  type: 'object',
-  properties: {
-    name: { type: 'string', minLength: 1, maxLength: 50 },
-    age: { type: 'number', minimum: 0 },
-    weightInKg: { type: 'number', minimum: 0 },
-  },
-  required: ['name', 'age', 'weightInKg'],
-  additionalProperties: false,
-} as const;
+import { getAllPetSchema } from './schemas/pets/get-all-pet.schemas';
+import { postPetSchema } from './schemas/pets/post-pet.schema';
 
 type RoutesDependencies = {
   petService: PetService;
 };
-
 export async function registerPetRoutes(
   app: FastifyInstance,
   dependencies: RoutesDependencies
 ) {
   const { petService } = dependencies;
 
-  app.get(
+  const typedApp = app.withTypeProvider<JsonSchemaToTsProvider>();
+
+  typedApp.get(
     '/api/pets',
-    { schema: { response: getAllPetResponseSchema } },
+    {
+      schema: getAllPetSchema,
+    },
     async () => {
       const pets = await petService.getAll();
       return pets;
     }
   );
 
-  app.post(
+  typedApp.post(
     '/api/pets',
     {
-      schema: {
-        body: petPostBodyRequestSchema,
-        response: petResponseSchema,
-      },
+      schema: postPetSchema,
     },
     async (request, reply) => {
-      const { body: petToCreate } = request;
-
-      //   const created = await petService.create(petToCreate);
+      const created = await petService.create(request.body);
       reply.status(201);
-      //   return created;
+      return created;
     }
   );
 }
